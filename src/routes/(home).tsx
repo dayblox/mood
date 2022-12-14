8b1bb6dcbcd004f7a0cec9a7750c8d25af8a2380
Raphaël QUEIROZ <rqueiroz-ext@coreoz.com>
2022-12-14 19:05:19 +0100

    title

--- src/routes/(home).tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<main>
-			<h1>Mood</h1>
-		</main>
-	)
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import solid from "solid-start/vite"
-import vercel from "solid-start-vercel"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [
-		solid({
-			adapter: vercel({ edge: true }),
-		}),
-	],
+	plugins: [solid()],
 })
